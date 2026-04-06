cce4041dc0bf87ae1efbd086d7da627a16ed38cc
Mariano Leyva <marianoleyva@Laptop-de-Mariano.local>
2026-04-05 23:30:26 -0600

    un pequeno cambio en la interfaz

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2699559603" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "453917145" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
